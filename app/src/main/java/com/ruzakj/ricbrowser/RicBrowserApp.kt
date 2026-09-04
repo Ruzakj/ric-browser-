b71fe1326f080494b3ee41b25f625cbf705d51a6
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-04 13:02:54 +0700

    Remove obsolete adblock engine initialization

--- app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
+++ app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
@@ -1,11 +1,5 @@
 package com.ruzakj.ricbrowser
 
 import android.app.Application
-import io.github.edsuns.adfilter.AdFilter
 
-class RicBrowserApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AdFilter.create(this)
-    }
-}
+class RicBrowserApp : Application()
