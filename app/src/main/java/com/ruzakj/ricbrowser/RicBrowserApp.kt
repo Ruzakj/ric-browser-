e5625fe69f23bc4cd04f6624f17ceff0264bfd94
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-08 14:26:33 +0700

    fix: wire direct MX Player media action

--- app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
+++ app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
@@ -10,7 +10,12 @@ class RicBrowserApp : Application() {
         registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
             override fun onActivityStarted(activity: Activity) = Unit
-            override fun onActivityResumed(activity: Activity) { activity.window.decorView.post { PowerTools.attach(activity) } }
+            override fun onActivityResumed(activity: Activity) {
+                activity.window.decorView.post {
+                    PowerTools.attach(activity)
+                    MediaDirectPatch.attach(activity)
+                }
+            }
             override fun onActivityPaused(activity: Activity) = Unit
             override fun onActivityStopped(activity: Activity) = Unit
             override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
