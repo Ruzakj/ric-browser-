4debd08bb04bd2935df0275bfcf94e7726b6d4a5
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-08 09:36:04 +0700

    feat: attach power tools through app lifecycle

--- app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
+++ app/src/main/java/com/ruzakj/ricbrowser/RicBrowserApp.kt
@@ -1,5 +1,20 @@
 package com.ruzakj.ricbrowser
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 
-class RicBrowserApp : Application()
+class RicBrowserApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+            override fun onActivityStarted(activity: Activity) = Unit
+            override fun onActivityResumed(activity: Activity) { activity.window.decorView.post { PowerTools.attach(activity) } }
+            override fun onActivityPaused(activity: Activity) = Unit
+            override fun onActivityStopped(activity: Activity) = Unit
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+            override fun onActivityDestroyed(activity: Activity) = Unit
+        })
+    }
+}
